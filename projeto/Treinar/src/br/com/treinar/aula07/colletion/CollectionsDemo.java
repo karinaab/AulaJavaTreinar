8108ee633e4cc0593bb7f191fd6f265c216a5cfa
ADM <ADM@MICRO121>
2017-01-20 22:03:47 -0200

    bla

--- projeto/Treinar/src/br/com/treinar/aula07/colletion/CollectionsDemo.java
+++ projeto/Treinar/src/br/com/treinar/aula07/colletion/CollectionsDemo.java
@@ -10,6 +10,16 @@ public class CollectionsDemo {
       a1.add("Ayan");
       System.out.println(" ArrayList Elements");
       System.out.print("\t" + a1);
+      
+   // ArrayList Generic (aceita apenas um tipo determinado tipo de váriavel)
+   // Evita casting 
+      List<String> a2 = new ArrayList<>();
+      a2.add("Zara");
+      a2.add("Mahnaz");
+      a2.add("Ayan");
+      //a2.add(10);  => não compilaria esta linha, porque o numero 10 não é String.
+      System.out.println(" ArrayList Elements");
+      System.out.print("\t" + a1);
 
       // LinkedList
       List l1 = new LinkedList();
@@ -30,11 +40,23 @@ public class CollectionsDemo {
       System.out.print("\t" + s1);
 
       // HashMap
+      //Cave, Valor
       Map m1 = new HashMap(); 
       m1.put("Zara", "8");
       m1.put("Mahnaz", "31");
       m1.put("Ayan", "12");
       m1.put("Daisy", "14");
+      System.out.println();
+      System.out.println(" Map Elements");
+      System.out.print("\t" + m1);
+      
+   // HashMap com determinação de tipo na chave e no valor.
+   //Cave, Valor
+      Map<Integer, Object> m2 = new HashMap<>(); 
+      m2.put(8, "Zara");
+      m2.put(32, "Pessoa");
+      //m2.put("Ayan", "12"); => não compilaria esta linha, porque o Ayan não é um Integer.
+      m2.put(3, "14");
       System.out.println();
       System.out.println(" Map Elements");
       System.out.print("\t" + m1);
--- projeto/Treinar/src/br/com/treinar/aula07/colletion/TesteMap.java
+++ projeto/Treinar/src/br/com/treinar/aula07/colletion/TesteMap.java
@@ -1,5 +1,17 @@
 package br.com.treinar.aula07.colletion;
 
-public class TesteMap {
+import java.util.HashMap;
+import java.util.Map;
 
+public class TesteMap {
+	public static void main(String[] args) {
+		Map<String, String> m1 = new HashMap<>();
+		m1.put("Zara", "8");
+		m1.put("Mahnaz", "31");
+		m1.put("Ayan", "12");
+		m1.put("Daisy", "14");
+
+		System.out.println("Map Elements");
+		System.out.println("\t" + m1);
+	}
 }
